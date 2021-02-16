ad728639bd8a56f2f714f8f71f55016e78e5c321
Matthew Hong <mjhong@sfu.ca>
2021-02-16 15:43:05 -0800

    made login template

--- SFUO/app.js
+++ SFUO/app.js
@@ -1,8 +1,17 @@
-var http = require('http');
-function onRequest(request, response){
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write("Hello SFUO");
-    response.end();
-}
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
-http.createServer(onRequest).listen(8000);
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/',require('./routes/index')); // must use this in tandem with router.get in index.js
+
+app.use('/users', require('./routes/users')); // to go to this link, go to users/login, users/register
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT,console.log('Server started on port ${PORT}'));
