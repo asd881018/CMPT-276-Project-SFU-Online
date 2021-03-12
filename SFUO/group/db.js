0ac576e20c003051494733d145359d2337a60edc
milak <milak@sfu.ca>
2021-03-11 16:55:57 -0800

    updated discussion board

--- SFUO/group/db.js
+++ SFUO/group/db.js
@@ -3,9 +3,9 @@ var { Pool } = require("pg");
 var Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "sfu",
+    user: "postgres",
     database: "sfu",
-    password: "sfu123",
+    password: "Fujiwara",
     host: "localhost",
     port: 5432
 });
